210c429f1d67dcb61e71167e5d221d8cf7b10baa
cotool <tusky.saga@gmail.com>
2020-08-29 02:08:22 +0800

    [加入]新增進資料庫功能

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -2,10 +2,26 @@
 const express = require('express')
 const router = express.Router()
 
+const Expense_tracker  = require('../../models/record')
+
 
 //新增頁面
 router.get('/new', (req, res) => {
   return res.render('new')
+})
+
+//新增到資料庫
+router.post('/', (req, res) => {
+  const { name, date,category, amount } = req.body
+  const expense = new Expense_tracker({
+    name,
+    date,
+    category,
+    amount
+  })
+  return expense.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 router.get('/index', (req,res) => {
