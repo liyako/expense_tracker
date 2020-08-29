ecabf15ffc5080564e1eff79099a86919195e7e4
cotool <tusky.saga@gmail.com>
2020-08-29 17:12:38 +0800

    [加入]使用者資料庫與註冊功能

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 //login
 router.get('/login', (req, res) => {
@@ -12,5 +13,28 @@ router.post('/login', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register')
 })
+
+// 註冊表單
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    User.findOne({ email }).then(user => {
+      if (user) {
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+  })
 
 module.exports = router
