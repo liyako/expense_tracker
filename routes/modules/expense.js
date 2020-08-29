d8052f709ef7ddafed0bfaf2bcf710d088103496
cotool <tusky.saga@gmail.com>
2020-08-29 15:27:56 +0800

    [加入]刪除、修改功能

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-const Expense_tracker  = require('../../models/record')
+const Record  = require('../../models/record')
 
 
 //新增頁面
@@ -13,7 +13,7 @@ router.get('/new', (req, res) => {
 //新增到資料庫
 router.post('/', (req, res) => {
   const { name, date,category, amount } = req.body
-  const expense = new Expense_tracker({
+  const expense = new Record({
     name,
     date,
     category,
@@ -23,6 +23,37 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+//資料修改頁面
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
 
+//資料修改到資料庫
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, date,category, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => res.render('error'))
+})
+//資料刪除
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 module.exports = router
