23d71973e5c89662232e648948d04bc9489fa0fe
cotool <tusky.saga@gmail.com>
2020-08-29 22:09:18 +0800

    [加入]message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const usePassport = require('./config/passport')
 
@@ -30,6 +31,17 @@ app.use(methodOverride('_method'))
 
 // 呼叫 Passport 函式並傳入 app
 usePassport(app)
+
+//flash提示窗
+app.use(flash())  // 掛載套件
+//middleware
+app.use((req,res,next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 //middleware
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
